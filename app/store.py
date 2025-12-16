1864c757641d7f1a9d1c897a7925c1e56f6258ec
hosna83 <rasihosna68@gmail.com>
2025-12-16 18:40:38 +0330

    feat: store core implementation

--- app/store.py
+++ app/store.py
@@ -1,5 +1,18 @@
-# - Implement main operations:
-#   - set(key, value) → store a value
-#   - get(key) → retrieve a value
-#   - delete(key) → remove a value
-#   - keys() → list all keys
+from typing import Dict, Any
+
+class KeyNotFoundError(Exception):
+    pass
+
+class InMemoryStore():
+    def __init__(self) -> None:
+        self._store: Dict[str, Any] = {}
+        
+    def set_value(self, key: str, value: Any):
+        self._store[key] = value
+        
+
+    def get_value(self, key: str) -> Any:
+        if key not in self._store:
+            raise KeyNotFoundError(f"key {key} is not found!")
+        
+        return self._store[key]
